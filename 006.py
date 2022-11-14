e4f723927c9687149fcd2ec119935491d5cb6165
Diego Chaves <79767758+chavesdiegoo@users.noreply.github.com>
2022-11-14 17:31:28 -0300

    Exercício 6 – Dobro, Triplo, Raiz Quadrada
    Exercício 6 – Dobro, Triplo, Raiz Quadrada

---
+++ 006.py
@@ -0,0 +1,5 @@
+n=float(input("Digite um numero: "))
+print('O dobro de {} é: {}' .format(n ,n*2))
+print('O triplo de {} é: {}' .format(n ,n*3))
+print('A Raiz quadrada de {} é: {}' .format(n, (n) ** 0.5))
+
