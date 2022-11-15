57bae6c641bf220f99ebe11ec23fe789416bf595
Diego Chaves <79767758+chavesdiegoo@users.noreply.github.com>
2022-11-15 19:06:54 -0300

    Exercício 17 – Catetos e Hipotenusa
    Exercício 17 – Catetos e Hipotenusa

--- 017.py
+++ 017.py
@@ -5,3 +5,10 @@ c1= float(input('Insira o comprimento do cateto oposto: '))
 c2 = float(input('Insira o comprimento do cateto adjacente: '))
 h= (c1**2)+(c2**2)
 print('A hipotenusa é {}'.format(sqrt(h)))
+
+#outrometodo
+
+from math import hypot
+c1= float(input('Insira o comprimento do cateto oposto: '))
+c2 = float(input('Insira o comprimento do cateto adjacente: '))
+print('A hipotenusa é {}'.format(hypot(c1, c2)))
