4f21d1858a28d19a7c696112ecd589710f3211f7
Diego Chaves <79767758+chavesdiegoo@users.noreply.github.com>
2022-11-15 12:43:45 -0300

    Exercício 7 – Média Aritmética
    Exercício 7 – Média Aritmética

---
+++ 007.py
@@ -0,0 +1,6 @@
+n=float(input("entre com a primeira nota: "))
+n1=float(input("Entre com a segunda nota: "))
+m= float (n+n1)/2
+print("A media entre {:.2f} e {:.2f}, é igual a {:.2f}".format(n, n1, m))
+14.9
+5.95
