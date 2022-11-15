b7dbe67126fa0299bd3dcacb0d2380437cee2191
Diego Chaves <79767758+chavesdiegoo@users.noreply.github.com>
2022-11-15 13:24:02 -0300

    Exercício 5 - Antecessor e Sucessor
    Exercício 5 - Antecessor e Sucessor

--- 005.py
+++ 005.py
@@ -2,5 +2,3 @@ n=int(input("Digite um numero: "))
 
 #print('Analisando o numero {}, o antecessor é {}, e o sucessor é {}: '.format(n, a, s))
 print('Analisando o numero {}, o antecessor é {}, e o sucessor é {}: '.format(n, n-1, n+1))
-
-
