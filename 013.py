e737ca9a3f9131c5d5d3d044c5c803d4e9551245
Diego Chaves <79767758+chavesdiegoo@users.noreply.github.com>
2022-11-15 14:41:42 -0300

    Exercício 13 – Reajuste Salarial
    Exercício 13 – Reajuste Salarial

--- 013.py
+++ 013.py
@@ -5,3 +5,11 @@ print('Considerando o bonus de 15% o novo salario do colaborador é :',s+(s*15/1
 s=float(input("Insira o valor do Salario do Funcionario R$: "))
 sa = s+(s*15/100)
 print('Um funcionario que recebe R${}, com um bonus de 15%, tem o novo salario de: {}'.format(s, sa))
+
+#Bonus
+p=float(input("Insira o valor do Preçp do produto  R$: "))
+pa= p-(p*5/100)
+pd= p+(p*8/100)
+print('O produto tem o valor de R${:.2f}. \n Para pagamento a vista o valor é {:.2f}, \n a prazo é de R${:.2f}'.format(p, pa, pd))
+
+
