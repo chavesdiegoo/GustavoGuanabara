620b215069d02e8d304a79ff2a6bcd9625821959
Diego Chaves <79767758+chavesdiegoo@users.noreply.github.com>
2022-11-15 14:58:50 -0300

    Exercício 14 – Conversor de Temperaturas
    Exercício 14 – Conversor de Temperaturas

---
+++ 014.py
@@ -0,0 +1,8 @@
+c=float(input("Insira a temperatura em graus Celsius: "))
+f =((9*c)/5)+32
+print('A temperatura de {:.2f}Cº convertida em Fahrenheit é de: {:.2f}Fº'.format(c, f))
+
+#outromodo
+
+t=float(input("Insira a temperatura em graus Celsius: "))
+print('A temperatura de {:.2f}Cº convertida em Fahrenheit é de: {:.2f}Fº'.format(t, ((9*t)/5)+32))
