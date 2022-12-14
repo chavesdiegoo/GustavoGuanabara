adab1fc88eab1b19f9c6eefdff51d29ff67a3717
Diego Chaves <79767758+chavesdiegoo@users.noreply.github.com>
2022-12-13 23:14:49 -0300

    Exercício Python 35:
    Exercício Python 35: Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.

---
+++ 035.py
@@ -0,0 +1,7 @@
+a = float(input('informe a 1 medida'))
+b = float(input('informe a 2 medida'))
+c= float(input('informe a 3 medida'))
+if a < b + c and b <a +c and c < a+b:
+    print('forma triangulo')
+else:
+    print('nao forma triangulo')
