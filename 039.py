d4733c0336c277b2f9aaaef5758dd9d5845155e2
Diego Chaves <79767758+chavesdiegoo@users.noreply.github.com>
2023-01-05 11:33:39 -0300

    Exercício Python 39:
    Exercício Python 39: Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.

--- 039.py
+++ 039.py
@@ -1,13 +1,18 @@
 from datetime import date
 agora = date.today().year
-ano = int(input('Informe o ano de nascimento do candango: '))
+ano = int(input('Informe o ano de nascimento: '))
 idade = agora - ano
 print('Quem nasceu em {}, tem hoje {} anos em {} .'.format(ano, idade, agora))
-if idade < 18:
-    resto = 18 - idade
-    print('Falta pouco, apenas {} anos '.format(resto))
-elif idade ==18:
+if idade ==18:
         print('Ta na hora')
+elif idade < 18:
+    resto = 18 - idade
+    c = agora + resto
+    print('Falta pouco, apenas {} anos '.format(resto))
+    print('Seu alistamento será em {}'.format(c))
 else:
     resto = idade - 18
+    c = agora - resto
     print('passou da idade, em {} anos '.format(resto))
+    print('Seu alistamento foi em {}'.format(c))
+
